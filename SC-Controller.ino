cd0162ea5d52d8fbd3b5bf312787097a9d1c5970
BeeBeetle <131170035+BeeBeetle@users.noreply.github.com>
2023-12-10 23:31:39 -0500

    using Bounce2
    Can now have the toggle switches accessed via a for loop without programming every one to treat them more like a moment button.

--- SC-Controller.ino
+++ SC-Controller.ino
@@ -1,38 +1,37 @@
-int buttonState = 0;
+#include <Bounce2.h>
+constexpr int NUM_TOGGLES = 2;
+constexpr int BOUNCE_INTER = 10;
+Bounce toggles[NUM_TOGGLES];
 
 void setup ()
 {
-  Joystick.useManualSend(true);
-  Serial.begin(38400);
-  pinMode(0, INPUT_PULLUP);
+  Joystick.useManualSend(true);  
+  for (int i = 0; i < NUM_TOGGLES; i++)
+  {
+    toggles[i].attach(i, INPUT_PULLUP);
+    toggles[i].interval(BOUNCE_INTER);
+  }
 }
 
 void loop ()
 {
-  if (digitalRead(0) == HIGH)
+  Joystick.hat(-1);
+  for (int i = 0; i < NUM_TOGGLES; i++)
   {
-    if (buttonState == 1)
+    toggles[i].update();
+    if (toggles[i].risingEdge())
     {
-      Joystick.button(1, 1);
-      buttonState = 0;
+      Joystick.button(i+1, 1);
+    }
+    else if (toggles[i].fallingEdge())
+    {
+      Joystick.button(i+1, 1);
     }
     else
     {
-      Joystick.button(1, 0);
+      Joystick.button(i+1, 0);
     }
   }
-  else
-  {
-    if (buttonState == 0)
-    {
-      Joystick.button(1, 1);
-      buttonState = 1;
-    }
-    else
-    {
-      Joystick.button(1, 0);
-    }
-  }
-  delay(100);
+  delay(50);
   Joystick.send_now();
 }
