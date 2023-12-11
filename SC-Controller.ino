7fc61cbe7a82b70f9d98a8f35fba8647b5e3afde
BeeBeetle <131170035+BeeBeetle@users.noreply.github.com>
2023-12-11 01:00:00 -0500

    Commented code

--- SC-Controller.ino
+++ SC-Controller.ino
@@ -1,13 +1,13 @@
 #include <Bounce2.h>
-constexpr int NUM_TOGGLES = 2;
-constexpr int BOUNCE_INTER = 10;
-Bounce toggles[NUM_TOGGLES];
+constexpr int NUM_TOGGLES = 2; // the number of toggle switches I'm using
+constexpr int BOUNCE_INTER = 10; // the ms delay over which the bounce is checked
+Bounce toggles[NUM_TOGGLES]; // sets an array with the number of toggles above
 
 void setup ()
 {
-  Joystick.useManualSend(true);  
-  for (int i = 0; i < NUM_TOGGLES; i++)
-  {
+  Joystick.useManualSend(true); // manually determine if the switch gets a signal
+  for (int i = 0; i < NUM_TOGGLES; i++) 
+  { // loops through the toggles to assign the switch types and add the interval
     toggles[i].attach(i, INPUT_PULLUP);
     toggles[i].interval(BOUNCE_INTER);
   }
@@ -15,23 +15,23 @@ void setup ()
 
 void loop ()
 {
-  Joystick.hat(-1);
+  Joystick.hat(-1); // for some reason I had a hat input, setting -1 "resets" the hat
   for (int i = 0; i < NUM_TOGGLES; i++)
-  {
-    toggles[i].update();
+  { // loop through the toggle switches and assigning them below to a joystick buttin starting at 1
+    toggles[i].update(); // check if their state (on/off) has changed
     if (toggles[i].risingEdge())
-    {
+    { // if the switch turns off (going from low to high) will give a signal to the PC
       Joystick.button(i+1, 1);
     }
     else if (toggles[i].fallingEdge())
-    {
+    { // if the switch turns on (going from high to low) will give a signal to the PC
       Joystick.button(i+1, 1);
     }
     else
-    {
+    { // if neither are true will give no signal
       Joystick.button(i+1, 0);
     }
   }
   delay(50);
-  Joystick.send_now();
+  Joystick.send_now(); // sends the joystick inputs recorded in the for loop
 }
